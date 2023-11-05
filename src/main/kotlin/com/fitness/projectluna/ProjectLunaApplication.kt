35cee7cc3fd787d408c0ca5613f48246a21fe68b
Marcelo <marcelo.leite@mercadolivre.com>
2023-11-05 19:31:16 -0300

    [feat] first commit

---
+++ src/main/kotlin/com/fitness/projectluna/ProjectLunaApplication.kt
@@ -0,0 +1,11 @@
+package com.fitness.projectluna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectLunaApplication
+
+fun main(args: Array<String>) {
+  runApplication<ProjectLunaApplication>(*args)
+}
